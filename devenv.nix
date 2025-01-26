adad160660fd2ec73719175a68097ed037005fa4
Yvaniak <pro@yvaniak.fr>
2025-01-26 10:47:17 +0100

    init

---
+++ devenv.nix
@@ -0,0 +1,47 @@
+{
+  pkgs,
+  lib,
+  config,
+  inputs,
+  ...
+}: {
+  languages.go.enable = true;
+
+  git-hooks.hooks = {
+    gofmt.enable = true;
+    golangci-lint.enable = true;
+    gotest.enable = true;
+    govet.enable = true;
+    revive.enable = true;
+    staticcheck.enable = true;
+
+    alejandra.enable = true;
+  };
+
+  env.GREET = "filesorter in go";
+
+  enterShell = ''
+    echo hello from $GREET
+  '';
+
+  enterTest = ''
+    echo "Running tests"
+  '';
+
+  outputs = {
+    filesort = pkgs.buildGoModule {
+      pname = "filesort";
+      version = "0.1.0";
+
+      src = ./.;
+
+      vendorHash = null;
+
+      meta = {
+        description = "simple filesorter in go for learning";
+        homepage = "https://github.com/Yvaniak/filesort";
+        licence = lib.licences.gpl3Plus;
+      };
+    };
+  };
+}
---
+++ filesort.go
@@ -0,0 +1,8 @@
+// Package filesort implement a simple filesorter, which goal is for me to learn go.
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello world")
+}
