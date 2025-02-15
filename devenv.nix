5dea0164e373708741aa438e0041880cb033f419
Yvaniak <pro@yvaniak.fr>
2025-02-15 19:03:43 +0100

    fix(devenv): change to use my new modules

--- devenv.nix
+++ devenv.nix
@@ -1,35 +1,11 @@
 {
-  pkgs,
+  inputs,
   ...
 }:
 {
-  languages.go.enable = true;
-  languages.nix.enable = true;
-
-  git-hooks.hooks = {
-    gofmt.enable = true;
-    golangci-lint.enable = true;
-    gotest.enable = true;
-    govet.enable = true;
-    revive.enable = true;
-    staticcheck.enable = true;
-    govulncheck = {
-      enable = true;
-      name = "govulncheck";
-      entry = "go run golang.org/x/vuln/cmd/govulncheck@latest ./...";
-      files = "\\.go$";
-      pass_filenames = false;
-    };
-
-    nixfmt-rfc-style.enable = true;
-    statix.enable = true;
-    deadnix.enable = true;
-    commitizen.enable = true;
-  };
-
-  enterShell = ''
-    echo hello from filesorter in go
-  '';
-
-  packages = [ pkgs.nil ];
+  imports = [
+    inputs.devenvs.homeManagerModules.devenvs.default
+  ];
+  go.enable = true;
+  nix.enable = true;
 }
